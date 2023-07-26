3059f4009a61d98fa7b0a265418725be96b09406
ramon <ramonjd@gmail.com>
2023-07-26 18:42:38 +1000

    Update tests

--- test/e2e/specs/site-editor/user-global-styles-revisions.spec.js
+++ test/e2e/specs/site-editor/user-global-styles-revisions.spec.js
@@ -84,8 +84,9 @@ test.describe( 'Global styles revisions', () => {
 			name: /^Changes saved by /,
 		} );
 
+		// There should be 2 revisions plus the added reset to theme defaults button.
 		await expect( revisionButtons ).toHaveCount(
-			currentRevisions.length + 2
+			currentRevisions.length + 3
 		);
 	} );
 
@@ -117,7 +118,7 @@ test.describe( 'Global styles revisions', () => {
 			.last()
 			.click();
 
-		await page.getByRole( 'button', { name: 'Apply' } ).click();
+		await page.getByRole( 'button', { name: 'Reset to defaults' } ).click();
 
 		const confirm = page.getByRole( 'dialog' );
 		await expect( confirm ).toBeVisible();
